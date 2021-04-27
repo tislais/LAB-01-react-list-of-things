9898365ae0fd3797958c6949cac6c3289a6870e6
Tis Lais <tistististis@gmail.com>
2021-04-27 13:41:27 -0700

    design search component

--- src/CreatureSearch.js
+++ src/CreatureSearch.js
@@ -5,7 +5,21 @@ export default class CreatureSearch extends Component {
   
   render() {
     return (
-      <form><button>🔎</button></form>
+      <div className="CreatureSearch">
+        <form>
+
+          <input />
+
+          <select>
+            <option value="">Sort...</option>
+            <option value="name">By Name</option>
+            <option value="lives">By Lives</option>
+          </select>
+          
+          <button>🔎</button>
+          
+        </form>
+      </div>
     );
   }
 }
